54f6bec02873db5b1ac6220536034aebd10fb68c
Sofus <sofusostrowska@gmail.com>
2021-10-24 23:31:25 +0200

    both 8 og 7 week stuff

--- 7uge/afl/rotate.fs
+++ 7uge/afl/rotate.fs
@@ -1,6 +1,7 @@
 module rotate
 type Position = int
 type Board = char list
+let rnd = System.Random ()
 
 let create (n : uint ) : Board =
     let lst = ['a' .. 'y']
@@ -12,36 +13,34 @@ let board2Str (b : Board) : string =
 // not pretty, but it works:)
 let validRotation (b : Board) (p : Position) : bool =
     match p with
-    | x1 when double p = sqrt (double (b.Length)) -> false
-    | x2 when double p = double 2 * sqrt (double (b.Length)) -> false
-    | x3 when double p = double 3 * sqrt (double (b.Length)) -> false
-    | x4 when double p = double 4 * sqrt (double (b.Length)) -> false
-    | x5 when double p > ((double b.Length) - (double (sqrt (double b.Length)))) -> false
+    | x1 when double p = double 1 + (sqrt (double (b.Length))) -> false
+    | x2 when double p = double 1 + (double 2 * sqrt (double (b.Length))) -> false
+    | x3 when double p = double 1 + (double 3 * sqrt (double (b.Length))) -> false
+    | x4 when double p = double 1 + (double 4 * sqrt (double (b.Length))) -> false
+    | x5 when double p > double 1 + ((double b.Length) - (double (sqrt (double b.Length)))) -> false
     | _ -> true
+
 
 let rotate (b : Board) (p : Position) : Board =
     let (boardSize : int) = int(sqrt(double b.Length))
-    if validRotation (b) (p+1) then
+    if validRotation (b) (p) then
         b.[0..(p-1)] @ (b.[p + boardSize] :: (b.[p] :: (b.[(p+2) .. (p+boardSize-1)] @ (b.[p+boardSize+1] :: (b.[p+1] :: (b.[(p+boardSize+2)..])))))) 
     else b
 
-let scramble (b : Board) (m : uint) : Board =
-    let rnd = System.Random ()
+let rec allowedRotation (b : Board) (p : Position) : Position =
+        let (boardSize : int) = int(sqrt(double b.Length))
+        if (validRotation (b) (p)) then p
+        else (allowedRotation (b) (rnd.Next (boardSize)))
+
+let rec scramble (b : Board) (m : uint) : Board =
     let (boardSize : int) = int(sqrt(double b.Length))
+    match m with
+    | 0u -> (rotate (b) (allowedRotation (b) (rnd.Next (boardSize))))
+    | _ -> (scramble2 (rotate (b) (allowedRotation (b) (rnd.Next (boardSize)))) (m-1u))
 
-    let rec allowedRotation (b : Board) (p : Position) : Position =
-        if validRotation (b) (rnd.Next (boardSize) 
-        then p
-        else allowedRotation (b) (rnd.Next (boardSize))
-
-    let rec scrambler (b : Board) (p : Position) (m : uint) : Board =
-        match m with
-        | 0u -> b
-        | _ -> (scrambler (rotate (b) (allowedRotation)) (allowedRotation) (m-1u)) 
-
-    scrambler (b) (allowedRotation) (m)
-
-        // | yes when validRotation (b) (p) -> (scrambler (rotate (b) (p)) (p) (m-1u))
-        // | _ -> scrambler (b) (rnd.Next (int(sqrt(double b.Length)))) (m) 
-    // scrambler (b) (rnd.Next (int(sqrt(double b.Length)))) (m)
-    
+let solved (b : Board) : bool =
+    let (boardSize : int) = int(sqrt(double b.Length))
+    let (solvedBoard : char list) = ['a' .. 'y']
+    match b with
+    | solvedBoard.[0 .. boardSize*boardSize] -> true
+    | _ -> false
